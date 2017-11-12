349d48b1e4f02ed9226615c94af6287a85d73add
Yi Fei Xue <yf3xue@uwaterloo.ca>
2017-11-12 13:35:25 -0500

    Initial commit. Vehicle movement working. working on gound check

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+	//public values
+	public float m_AccelerationValue;
+	public float m_MaxSpeed;
+	public float m_TurningValue;
+	//private values
+	private float m_ThrustInput;
+	private float m_TurnInput;
+	private Rigidbody m_RigidBody;
+	private GroundCheck m_GroundCheck;
+	// Use this for initialization
+	void Start () {
+		m_RigidBody = GetComponent<Rigidbody>();
+		m_GroundCheck = GetComponent<GroundCheck>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_ThrustInput = Input.GetAxis("Vertical");
+		m_TurnInput = Input.GetAxis("Horizontal");
+	}
+	void ApplyThrust(){
+		m_RigidBody.AddRelativeForce(new Vector3(0, 0, m_AccelerationValue * m_ThrustInput));
+	}
+	void ApplyTurning(){
+		Vector3 forwardVelocity = transform.InverseTransformDirection(m_RigidBody.velocity);
+		transform.Rotate(Vector3.up * m_TurnInput * m_TurningValue * forwardVelocity.z * Time.deltaTime);
+	}
+	void LimitVelocity(){
+		Vector3 velocity = m_RigidBody.velocity;
+		if(velocity.magnitude > m_MaxSpeed){
+			velocity = velocity.normalized * m_MaxSpeed;
+			m_RigidBody.velocity = velocity;
+		}
+	}
+	/// <summary>
+	/// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void FixedUpdate()
+	{
+		if(m_GroundCheck.IsGrounded){
+			ApplyThrust();
+			LimitVelocity();
+			ApplyTurning();
+		}
+	}
+}
---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+	public float m_Clearance;
+	public BoxCollider m_ColliderBody;
+	public bool IsGrounded{
+		get{
+			return m_isGrounded;
+		}
+	}
+	private bool m_isGrounded = false;
+	private Vector3 m_BoxCastCentre;
+	private Vector3 m_HalfExtents;
+	// Use this for initialization
+	void Start () {
+		Vector3 scaledSize = Vector3.Scale(m_ColliderBody.size, m_ColliderBody.transform.localScale);
+
+		m_BoxCastCentre = m_ColliderBody.center;
+		m_BoxCastCentre.y -= scaledSize.y * 0.5f;
+
+		m_HalfExtents = scaledSize;
+		m_HalfExtents.y = m_Clearance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	void FixedUpdate()
+	{
+		m_isGrounded = Physics.BoxCast(transform.position + m_BoxCastCentre, m_HalfExtents, Vector3.up);
+	}
+	/// <summary>
+	/// Callback to draw gizmos only if the object is selected.
+	/// </summary>
+	void OnDrawGizmos()
+	{
+		if(m_isGrounded){
+			Gizmos.color = new Color(1, 1, 1, 1.0F);
+
+		}
+		else{
+			Gizmos.color = new Color(0, 0, 0, 1.0F);
+		}
+        Gizmos.DrawCube(transform.position + m_BoxCastCentre, m_HalfExtents);
+	}
+}
